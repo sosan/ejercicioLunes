7c2a96a9c1548568a2fc7c55d3a9a5322c91f6f1
jose <josloe@gmail.com>
2019-09-30 20:52:02 +0200

    gggggggg

--- servidor/forms.py
+++ servidor/forms.py
@@ -1,17 +1,27 @@
-from wtforms import Form, StringField, TextField, validators
+from wtforms import Form, StringField, TextField, validators, HiddenField
 from wtforms.fields.html5 import EmailField
+
+
+# crear una validacion propia
+def honeyPot(form, field):
+    if len(field.data) > 0:
+        raise validators.ValidationError("El campo esta vacio")  # siginifca que es un robot
 
 
 class formularioContactar(Form):  # FlaskForm
     nombre = StringField("nombre:", validators=[
+        validators.required("Estas obligado a rellenar este campo"),
         validators.length(min=3, max=25, message="Nombre incorrecto")
     ])
     email = EmailField("email: ", validators=[
-        validators.length(min=5, max=25)
-
+        validators.required("Este campo es requerido"),
+        validators.email("Ingrese un email: ")
 
     ])
     comentario = TextField("comentario")
+
+    # señuelo
+    sunyuelo = HiddenField("", {honeyPot})
 
 # def formularioContactar():
 #     formulario = TemplateFormLogin()
